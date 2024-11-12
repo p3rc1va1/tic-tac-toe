b6f6159d119899f7554573d401c33d636009d1a0
p3rc1va1 <35celik.baha@gmail.com>
2024-11-12 19:30:11 +0200

    terminal version is done

--- game.js
+++ game.js
@@ -1,23 +1,104 @@
+const prompt = require('prompt-sync')();
 //use factories and make it as clean as possible
 
 
 //there should be a scoreboard
-function gameBoard (winner) {
-    let xWins = () =>{ xScore++};
-    let oWins = () =>{ oScore++};
+function gameBoard() {
+    let xScore = 0;
+    let oScore = 0;
+
+    // Return an object with methods to access and modify the scores
+    return {
+        xWins: () => { xScore++ },
+        oWins: () => { oScore++ },
+        getScores: () => ({ xScore, oScore })  // A method to see the current scores
+    };
 }
+
 
 function gamePlay(){
     let gameContinues = true;
-    let rounds = 0;
+    let rounds = 1;
     let coordinatesAvailable = ['a','b','c',
-                       'd','e','f',
-                       'g','h','i'];
-    while(gameContinues && rounds <=9){
-        let xMove = prompt(`Which coordinate would you like to mark "X" with?//aaaa`);
-        console.log(xMove)
+                                'd','e','f',
+                                'g','h','i'];
+    while(gameContinues && rounds <=4){
+        let xMove;
+        let oMove;
+        //wait for a valid move
+        do{
+        xMove = prompt("What is your move X?")
+        }while(!(coordinatesAvailable.includes(xMove)));
+
+        //delete the selected coordinate
+        coordinatesAvailable.push("X"+coordinatesAvailable.splice(coordinatesAvailable.indexOf(xMove),1));
+        console.log(coordinatesAvailable)
+
+        if(rounds>=2 &&
+            (coordinatesAvailable.includes("Xa", -(rounds*2))&& coordinatesAvailable.includes("Xb", -(rounds*2))&& coordinatesAvailable.includes("Xc", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xd",-(rounds*2))&& coordinatesAvailable.includes("Xe", -(rounds*2))&& coordinatesAvailable.includes("Xf", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xg",-(rounds*2))&& coordinatesAvailable.includes("Xh", -(rounds*2))&& coordinatesAvailable.includes("Xi", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xa",-(rounds*2))&& coordinatesAvailable.includes("Xd", -(rounds*2))&& coordinatesAvailable.includes("Xg", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xb",-(rounds*2))&& coordinatesAvailable.includes("Xe", -(rounds*2))&& coordinatesAvailable.includes("Xh", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xc",-(rounds*2))&& coordinatesAvailable.includes("Xf", -(rounds*2))&& coordinatesAvailable.includes("Xi", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xa",-(rounds*2))&& coordinatesAvailable.includes("Xe", -(rounds*2))&& coordinatesAvailable.includes("Xi", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Xg",-(rounds*2))&& coordinatesAvailable.includes("Xe", -(rounds*2))&& coordinatesAvailable.includes("Xc", -(rounds*2)))
+
+        ){
+            console.log("X won!!!")
+            session.xWins()
+            console.log("Curren score: ", session.getScores())
+            break
+        }
+        //wait for a valid move
+        do{
+        oMove = prompt("What is your move O?")
+        }while(!(coordinatesAvailable.includes(oMove)));
+
+        //delete the selected coordinate
+        coordinatesAvailable.push("O"+coordinatesAvailable.splice(coordinatesAvailable.indexOf(oMove),1));
+        console.log(coordinatesAvailable)
+        
+        if(rounds>=2 &&
+            (coordinatesAvailable.includes("Oa", -(rounds*2))&& coordinatesAvailable.includes("Ob", -(rounds*2))&& coordinatesAvailable.includes("Oc", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Od",-(rounds*2))&& coordinatesAvailable.includes("Oe", -(rounds*2))&& coordinatesAvailable.includes("Of", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Og",-(rounds*2))&& coordinatesAvailable.includes("Oh", -(rounds*2))&& coordinatesAvailable.includes("Oi", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Oa",-(rounds*2))&& coordinatesAvailable.includes("Od", -(rounds*2))&& coordinatesAvailable.includes("Og", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Ob",-(rounds*2))&& coordinatesAvailable.includes("Oe", -(rounds*2))&& coordinatesAvailable.includes("Oh", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Oc",-(rounds*2))&& coordinatesAvailable.includes("Of", -(rounds*2))&& coordinatesAvailable.includes("Oi", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Oa",-(rounds*2))&& coordinatesAvailable.includes("Oe", -(rounds*2))&& coordinatesAvailable.includes("Oi", -(rounds*2)))
+            ||
+            (coordinatesAvailable.includes("Og",-(rounds*2))&& coordinatesAvailable.includes("Oe", -(rounds*2))&& coordinatesAvailable.includes("Oc", -(rounds*2)))
+
+        ){
+            console.log("O won!!!")
+            session.oWins()
+            console.log("Curren score: ", session.getScores())
+            break
+        }
+
+
+        
+
+        rounds++
     }
     
-}
+};
 
-gamePlay()
+const session = gameBoard()
+while (true){
+    gamePlay()
+}
